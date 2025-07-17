90b6cd99f1103c0a5a216f682ad13dbcf2951fdd
IktaS <imamrafiia@gmail.com>
2025-07-17 14:39:19 +0700

    change waybar

--- hosts/ikta/variables.nix
+++ hosts/ikta/variables.nix
@@ -73,12 +73,7 @@
 
   # Set Waybar
   #  Available Options:
-  waybarChoice = ../../modules/home/waybar/waybar-curved.nix;
-  #waybarChoice = ../../modules/home/waybar/waybar-ddubs.nix;
-  #waybarChoice = ../../modules/home/waybar/waybar-ddubs-2.nix;
-  #waybarChoice = ../../modules/home/waybar/waybar-simple.nix;
-  #waybarChoice = ../../modules/home/waybar/Jerry-waybar.nix;
-  #waybarChoice = ../../modules/home/waybar/waybar-nekodyke.nix;
+  waybarChoice = ../../modules/home/waybar/waybar-ddubs-2.nix;
 
   # Set Animation style
   # Available options are:

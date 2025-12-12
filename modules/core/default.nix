38da401e6ed98062bdc353e96254a12f11a21e37
Don Williams <don.e.williams@gmail.com>
2025-12-12 09:27:00 -0500

    Changed to LY greeter for TUI option
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   modules/core/default.nix
    	new file:   modules/core/ly.nix

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -20,7 +20,7 @@ in {
     # Conditionally import the display manager module
     (
       if vars.displayManager == "tui"
-      then ./greetd.nix
+      then ./ly.nix
       else ./sddm.nix
     )
     ./security.nix

6ac33da452e42fef90f2ee29f73713be03dc16fe
Don Williams <don.e.williams@gmail.com>
2025-12-12 09:34:12 -0500

    Porting QS scripts  ddubsos to zaneyos for waybars
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   modules/home/scripts/default.nix
    	new file:   modules/home/scripts/qs-vid-wallpapers-apply.nix
    	new file:   modules/home/scripts/qs-vid-wallpapers.nix
    	new file:   modules/home/scripts/qs-wallpapers-apply.nix
    	new file:   modules/home/scripts/qs-wallpapers-restore.nix
    	new file:   modules/home/scripts/qs-wallpapers.nix
    	new file:   modules/home/scripts/qs-wlogout-icons/hibernate.png
    	new file:   modules/home/scripts/qs-wlogout-icons/lock.png
    	new file:   modules/home/scripts/qs-wlogout-icons/logout.png
    	new file:   modules/home/scripts/qs-wlogout-icons/reboot.png
    	new file:   modules/home/scripts/qs-wlogout-icons/shutdown.png
    	new file:   modules/home/scripts/qs-wlogout-icons/suspend.png
    	new file:   modules/home/scripts/qs-wlogout.nix

--- modules/home/scripts/default.nix
+++ modules/home/scripts/default.nix
@@ -24,6 +24,13 @@
     # Cheatsheets viewer + parser
     (import ./cheatsheets-parser.nix {inherit pkgs;})
     (import ./qs-cheatsheets.nix {inherit pkgs;})
+    # QuickShell scripts
+    (import ./qs-vid-wallpapers.nix {inherit pkgs;})
+    (import ./qs-vid-wallpapers-apply.nix {inherit pkgs;})
+    (import ./qs-wallpapers.nix {inherit pkgs;})
+    (import ./qs-wallpapers-apply.nix {inherit pkgs;})
+    (import ./qs-wallpapers-restore.nix {inherit pkgs;})
+    (import ./qs-wlogout.nix {inherit pkgs;})
     (import ./zcli.nix {
       inherit pkgs profile;
       backupFiles = [

23bdd107f891180e75edff0259d04aa0d66cfcea
IktaS <imamrafiia@gmail.com>
2026-03-10 20:34:35 +0700

    fix zen browser

--- modules/home/zen-browser.nix
+++ modules/home/zen-browser.nix
@@ -3,13 +3,16 @@
   inputs,
   ...
 }:
-let
-  # Prefer explicit package name if available; fall back to default
-  zenPkg =
-    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.zen-browser
-      or inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default;
-in
 {
-  # Install Zen Browser for the user
-  home.packages = [ zenPkg ];
+  imports = [
+    inputs.zen-browser.homeModules.beta
+  ];
+
+  programs.zen-browser.enable = true;
+
+  programs.zen-browser.profiles.default.extensions.packages =
+    with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
+        yomitan
+        adnauseam
+      ];
 }

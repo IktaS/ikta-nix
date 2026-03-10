5addc89ba3ff5b5109d1e56fe2240661635a5132
IktaS <imamrafiia@gmail.com>
2026-03-10 23:08:49 +0700

    add steam extraEnv

--- modules/core/steam.nix
+++ modules/core/steam.nix
@@ -8,19 +8,21 @@
       gamescopeSession.enable = true;
       extraCompatPackages = [ pkgs.proton-ge-bin ];
       extest.enable = true;
-      # extraEnv = {
-      #   MANGOHUD = "1";
-      #   MANGOHUD_CONFIG = "read_cfg,no_display";
-      #   GAMEMODERUN = "1";
-      #   AMD_VULKAN_ICD = "RADV";
-      #   VKD3D_CONFIG = "dxr,dxr11";
-      #   PROTON_ADD_CONFIG = "fsr4rdna3";
-      #   PROTON_LOCAL_SHADER_CACHE = "1";
-      #   MESA_SHARDER_CACHE_MAX_SIZE = "16G";
-      #   WINE_VK_VULKAN_ONLY = "1";
-      #   MESA_GLSL_CACHE_MAX_SIZE = "16G";
-      #   WIDEDLLOVEERRIDES = "diinput8,dxgi,dsound=n,b";
-      # };
+      package = pkgs.steam.override {
+        extraEnv = {
+          MANGOHUD = "1";
+          MANGOHUD_CONFIG = "read_cfg,no_display";
+          GAMEMODERUN = "1";
+          AMD_VULKAN_ICD = "RADV";
+          VKD3D_CONFIG = "dxr,dxr11";
+          PROTON_ADD_CONFIG = "fsr4rdna3";
+          PROTON_LOCAL_SHADER_CACHE = "1";
+          MESA_SHARDER_CACHE_MAX_SIZE = "16G";
+          WINE_VK_VULKAN_ONLY = "1";
+          MESA_GLSL_CACHE_MAX_SIZE = "16G";
+          WIDEDLLOVEERRIDES = "diinput8,dxgi,dsound=n,b";
+        };
+      };
     };
 
     gamescope = {
